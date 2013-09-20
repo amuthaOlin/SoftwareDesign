936733a43ec095db76eb4b9b37546ef593cf615e
Ankeet Mutha <ankeet.mutha@students.olin.edu>
2013-09-20 11:47:26 -0400

    adding arc program

---
+++ chap04/Polygon.py/arc.py
@@ -0,0 +1,20 @@
+from swampy.TurtleWorld import*
+#draws an arc with radius "r" and angle "angle" 
+#"t" is a turtle.
+
+world = TurtleWorld()
+bob = Turtle()
+import math
+
+def arc(t,r,angle):
+	bob.delay=.01
+	c = math.pi*r**2 
+	l = c/360.0
+	print t
+	for i in range(angle):
+		fd(t,l)
+		lt(t,1)
+
+	wait_for_user
+
+arc(bob,10.0,180)
